4b55c6f5f1052cc42e2e6e12a2872ff12df639b4
alexandre <alexandre1alves@gmail.com>
2025-02-15 19:51:03 -0300

    correção .env

--- server.js
+++ server.js
@@ -85,6 +85,11 @@ app.delete("/usuarios/:id", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+// app.listen(3000, () => {
+//   console.log("Server is running on port 3000");
+// });
+app.listen(process.env.PORT || 3000, () => {
+
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
+
 });
